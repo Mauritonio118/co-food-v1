46d67e33fe4d00961a1489e383cf67b6733a6365
Mauritonio118 <mauro.ojeda.f@gmail.com>
2025-05-31 10:04:48 -0400

    Actualizada la configuración de Next.js para permitir orígenes de desarrollo dinámicos y se modificó la acción de verificación en el componente Verify para utilizar 'justverify'.

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['static.usernames.app-backend.toolsforhumanity.com'],
   },
-  allowedDevOrigins: ['*'], // Add your dev origin here
+  allowedDevOrigins: [(process.env.AUTH_URL ?? '').slice(8)], // Add your dev origin here
   reactStrictMode: false,
 };
 
